d2fa48176bbcd2ee638061d42cd4d75557afc4be
Paul Wilson <paulanthonywilson@gmail.com>
2020-06-12 18:57:09 +0100

    redirect http to https

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -162,8 +162,12 @@ resource "aws_lb_listener" "http" {
   port = "80"
   protocol = "HTTP"
   default_action {
-    target_group_arn = aws_lb_target_group.instance.arn
-    type = "forward"
+    type = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
